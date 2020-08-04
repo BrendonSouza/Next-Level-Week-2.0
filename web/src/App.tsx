d7404bd8a543b171dd9a28c6a9db0c9c04d69c9c
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-08-03 22:32:30 -0300

    dia 01 - Preparar ambiente, node, yarn, expo, primeiras páginas

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Routes from './routes';
+import './assets/styles/global.css'
+//JSX - javascript com xml
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
