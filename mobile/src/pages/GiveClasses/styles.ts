f193a71f2f00a3ef7dde75be16484d78d8eae390
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-08-07 15:38:20 -0300

    dia 04.1 - finalizando a parte mobile do 4º dia do evento

--- mobile/src/pages/GiveClasses/styles.ts
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -11,9 +11,39 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     justifyContent: 'center',
-    resizeMode:'contain'
-  }
+  },
+  
+  title: {
+    fontFamily:'Archivo_700Bold',
+    color:'#FFF',
+    fontSize:32,
+    lineHeight: 37,
+    maxWidth: 180,
+  },
 
+  description: {
+    marginTop: 24,
+    color:'#d4c2ff',
+    fontSize: 16,
+    lineHeight: 26,
+    fontFamily: 'Poppins_400Regular',
+    maxWidth: 240
+  },
+
+  okButton: {
+    marginVertical: 40,
+    backgroundColor: '#04d361',
+    height: 58,
+    alignItems:'center',
+    justifyContent:'center',
+    borderRadius:8
+  },
+
+  okButtonText: {
+    color: '#FFF',
+    fontSize: 18,
+    fontFamily: 'Archivo_700Bold'
+  }
     
 })
 
